12e228ffef9e51e3b8fe0d67159461faee3a95e8
davisonrails <coverdavis@gmail.com>
2018-02-12 22:09:49 -0500

    Adding PC Python Files

---
+++ dieChallenge.py
@@ -0,0 +1,36 @@
+import random
+
+score = 0
+
+wants_to_play = True
+
+def play_game():
+    global die_roll
+    die_roll = random.randint(1, 6)
+    global first_question
+    first_question = int(input("What do you think the roll will be?"))
+    check_correct()
+    check_for_want()
+
+def check_correct():
+    if first_question == die_roll:
+        print("correct!")
+    else:
+        print("not correct!")
+
+def check_for_want():
+    global answer
+    answer = input("Do you want to try again?")
+    if answer == "yes":
+        print("ok!")
+    else:
+        global wants_to_play
+        wants_to_play = False
+
+
+while wants_to_play == True:
+    play_game()
+
+
+
+
---
+++ quadratics.py
@@ -0,0 +1,17 @@
+import math
+
+a = int(input("Enter the coefficients of a: "))
+b = int(input("Enter the coefficients of b: "))
+c = int(input("Enter the coefficients of c: "))
+
+d = b**2-4*a*c
+
+if d < 0:
+    print ("This equation has no real solution")
+elif d == 0:
+    one = (-b+math.sqrt(b**2-4*a*c))/2*a
+    print ("This equation has one solutions: "), d
+else:
+    pos = (-b+math.sqrt((b**2)-(4*(a*c))))/(2*a)
+    neg = (-b-math.sqrt((b**2)-(4*(a*c))))/(2*a)
+    print ("This equation has two solutions: ", pos, " or", neg)
