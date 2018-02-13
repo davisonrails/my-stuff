268db58a6000e6d896b8daccd851f0ec3ccda8e6
davisonrails <coverdavis@gmail.com>
2018-02-12 22:20:15 -0500

    Update haha.py

--- haha.py
+++ haha.py
@@ -1,9 +1,11 @@
+#I made this last November that I ran on a friend's computer that randomly opened a website every 9-18 seconds, was not amused 
+
 import webbrowser
 import time
 import random
 
 while True:
-	sites = random.choice(['cornhub.com', 'beesbeesbees.com', 'urbandictionary.com/define.php?term=goofed', 'en.wikipedia.org/wiki/Goofball'])
+	sites = random.choice(['beesbeesbees.com', 'urbandictionary.com/define.php?term=goofed', 'en.wikipedia.org/wiki/Goofball'])
 	visit = "http://{}".format(sites)
 	webbrowser.open(visit)
 	seconds = random.randrange(9 , 18)
