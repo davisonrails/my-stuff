5cc90d6d57d232bf5803131ad585584c0698709a
davisonrails <coverdavis@gmail.com>
2018-02-12 22:12:41 -0500

    CS Projects 2/12

---
+++ Rectangle.java
@@ -0,0 +1,45 @@
+public class Rectangle {
+    
+    private double length;
+    private double width;
+    private double perimeter;
+    private double area;
+    
+    public Rectangle(){ }
+    
+    public Rectangle(double l, double w) {
+        length = l;
+        width = w;
+    }
+    
+    public double getLength(){
+        return length;
+    }
+    
+    public double getWidth(){
+        return width;
+    }
+    
+    public void setLength(double s){
+        length = s;
+    }
+    
+    public void setWidth(double s){
+        width = s;
+    }
+    
+    public double getArea(){
+        area = length * width;
+        return area;
+    }
+    
+    public double getPerimeter(){
+        perimeter = (length + width) * 2;
+        return perimeter;
+    }
+
+    public String toString(){
+        return "Rectangle: Length = " + length + " Width = " + width + " Perimeter = " + perimeter + " Area = " + area;
+    }
+    
+}
---
+++ RectangleDriver.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class RectangleDriver {
+    public static void main(String []args){
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter length: ");
+        double localLength = scan.nextDouble();
+        
+        System.out.println("Enter width: ");
+        double localWidth = scan.nextDouble();
+        
+        Rectangle r = new Rectangle(localLength, localWidth);
+        
+        System.out.println("\nUser Created Rectangle with Overloaded Constructor");
+        System.out.println("The area is: " + r.getArea());
+        System.out.println("The perimeter is: " + r.getPerimeter());
+        
+        Rectangle r2 = new Rectangle();
+        r2.setLength(1);
+        r2.setWidth(8);
+       
+        System.out.println("\nRectangle r2 = new Triangle() default constructor information");
+        System.out.println("The area is: " + r2.getArea());
+        System.out.println("The perimeter is: " + r2.getPerimeter());
+        System.out.println(r2.toString());
+        
+        Rectangle r3 = new Rectangle(1.2, 3.6);
+        System.out.println("\nUser Created Rectangle with Overloaded Constructor");
+        System.out.println("The area is: " + r3.getArea());
+        System.out.println("The perimeter is: " + r3.getPerimeter());
+        System.out.println(r3.toString());
+        
+                               
+    }
+}
