30d68d467b8083e39ef9e06d99862a9680bb8f6a
davisonrails <coverdavis@gmail.com>
2018-02-12 22:16:34 -0500

    Update goofed.js

--- goofed.js
+++ goofed.js
@@ -1,3 +1,6 @@
+/*I made this to mess with my friend so that whenever he opens 
+his browser the entire page would always delete (through more code that I don't have here*/
+
 function goofed() {
 
   var siteToGoof = document.querySelector('body');
